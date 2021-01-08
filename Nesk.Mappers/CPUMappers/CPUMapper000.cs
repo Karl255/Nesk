4595474394faf0595c1f69b7c5cc92ff19366d1d
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-01-08 10:11:45 +0100

    Completely implemented mapper 000

--- Nesk.Mappers/CPUMappers/CPUMapper000.cs
+++ Nesk.Mappers/CPUMappers/CPUMapper000.cs
@@ -5,33 +5,44 @@ namespace Nesk.Mappers.CPUMappers
 	public class CPUMapper000 : CPUMapper
 	{
 		private byte[] PrgRom;
-		private int AddressMask = 0;
+		private int PrgRomAddressMask;
+		private byte[] PrgRam;
+		private int PrgRamAddressMask;
+
 
 		public CPUMapper000(Cartridge cartridge)
 		{
-			// make sure the PRG-ROM size is 8k or 16k and set the appropriate mask (for mirroring when size is 8k)
-			if (cartridge.PrgRom.Length == 16 * 1024) // 16k
-				AddressMask = 0x3fff;
-			else if (cartridge.PrgRom.Length == 8 * 1024) // 32k
-				AddressMask = 0x7fff;
-			else
+			// make sure the PRG-ROM size is 8k or 16k
+			if (cartridge.PrgRom.Length != 16 * 1024    // 16k
+				|| cartridge.PrgRom.Length != 8 * 1024) // 32k
 				throw new Exception($"Malformed ROM file: invalid PRG-ROM size, expectd 16384 or 32768, got {cartridge.PrgRom.Length}");
 
 			PrgRom = cartridge.PrgRom;
+			// for mirroring
+			PrgRomAddressMask = PrgRom.Length - 1;
 
-			/*
-			if (cartridge.ChrRomSize != 0x2000) // 8k
-				throw new Exception($"Malformed ROM file: invalid CHR-ROM size ({cartridge.ChrRom.Length})");
-
-			ChrRom = cartridge.ChrRom;
-			*/
+			// same for PRG-ROM
+			if (cartridge.HasPrgRam)
+			{
+				PrgRam = new byte[cartridge.PrgRamSize];
+				PrgRamAddressMask = PrgRam.Length - 1;
+			}
 		}
 
-		public new byte this[int address] => address switch
+		public new byte this[int address]
 		{
-			>= 0x0000 and <= 0x7fff => base[address],
-			>= 0x8000 and <= 0xffff => PrgRom[(address - 0x8000) & AddressMask],
-			_ => 0
-		};
+			get => address switch
+			{
+				>= 0x6000 and <= 0x7fff => PrgRam != null ? PrgRam[(address - 0x6000) & PrgRamAddressMask] : 0, // PRG-RAM
+				>= 0x8000 and <= 0xffff => PrgRom[(address - 0x8000) & PrgRomAddressMask],                      // PRG-ROM
+				_ => base[address],
+			};
+
+			set => _ = address switch
+			{
+				>= 0x6000 and <= 0x7fff => PrgRam != null ? PrgRam[(address - 0x6000) & PrgRamAddressMask] = value : 0, // PRG-RAM
+				_ => base[address] = value
+			};
+		}
 	}
 }
